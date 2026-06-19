37856220c0d137c3c2581f5ca056da12acc7ec6d
Gokul <gokulneethi@gmail.com>
2026-06-19 08:47:39 +0530

    backend update

--- backend/main.py
+++ backend/main.py
@@ -78,7 +78,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.origins_list + ["*"],
+        allow_origins=settings.origins_list,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
